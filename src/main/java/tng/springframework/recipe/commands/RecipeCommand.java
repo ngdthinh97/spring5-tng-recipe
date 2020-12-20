a345ea29efd39c33c76faf68775ac3886462c1e1
ngdthinh97 <ngdthinh.97@gmail.com>
2020-12-20 13:01:56 +0700

    Modified Test for setup controller ad and hibernate for check

--- src/main/java/tng/springframework/recipe/commands/RecipeCommand.java
+++ src/main/java/tng/springframework/recipe/commands/RecipeCommand.java
@@ -1,4 +1,5 @@
 package tng.springframework.recipe.commands;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -7,22 +8,22 @@ import tng.springframework.recipe.domain.Difficulty;
 import java.util.HashSet;
 import java.util.Set;
 
-
 @Getter
 @Setter
 @NoArgsConstructor
 public class RecipeCommand {
-	   private Long id;
-	    private String description;
-	    private Integer prepTime;
-	    private Integer cookTime;
-	    private Integer servings;
-	    private String sourse;
-	    private String url;
-	    private String directions;
-	    private Set<IngredientCommand> ingredients = new HashSet<>();
-	    private Byte[] image;
-	    private Difficulty difficulty;
-	    private NotesCommand notes;
-	    private Set<CategoryCommand> categories = new HashSet<>();
+	private Long id;
+
+	private String description;
+	private Integer prepTime;
+	private Integer cookTime;
+	private Integer servings;
+	private String sourse;
+	private String url;
+	private String directions;
+	private Set<IngredientCommand> ingredients = new HashSet<>();
+	private Byte[] image;
+	private Difficulty difficulty;
+	private NotesCommand notes;
+	private Set<CategoryCommand> categories = new HashSet<>();
 }
--- src/test/java/tng/springframework/recipe/controllers/RecipeControllerTest.java
+++ src/test/java/tng/springframework/recipe/controllers/RecipeControllerTest.java
@@ -1,39 +1,50 @@
 package tng.springframework.recipe.controllers;
 import static org.mockito.Mockito.*;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import static org.mockito.ArgumentMatchers.anyLong;
-import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import tng.springframework.Exceptions.NotFoundExceptions;
 import tng.springframework.recipe.domain.Recipe;
+import tng.springframework.recipe.repositories.RecipeRepository;
 import tng.springframework.recipe.services.RecipeService;
 
+@ExtendWith(MockitoExtension.class)
 public class RecipeControllerTest {
 
 	@Mock
-	@InjectMocks
 	RecipeService recipeService;
 
+	@InjectMocks
 	RecipeController controller;
+	
+	@Mock
+	RecipeRepository recipeRepository;
 
 	MockMvc mockMvc;
 
-	@Before
+	@BeforeEach
 	public void setUp() throws Exception {
 		MockitoAnnotations.initMocks(this);
 
 		controller = new RecipeController(recipeService);
-		mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+		mockMvc = MockMvcBuilders.standaloneSetup(controller)
+				.setControllerAdvice(new ControllerExceptionHandler())
+				.build();
 	}
+	
     @Test
     public void testGetRecipeNotFound() throws Exception {
 
@@ -48,23 +59,22 @@ public class RecipeControllerTest {
 
 		Recipe recipe = new Recipe();
 		recipe.setId(1L);
-		recipeService.deleteById(recipe.getId());
-		Recipe recipeid = recipeService.findById(Long.valueOf(1));
-		Long id = recipeid.getId();
-		System.out.println();
+		String kq = (recipe.getId().toString().equals("1")) ? "yes" : "N/A";
+		
 		when(recipeService.findById(anyLong())).thenReturn(recipe);
 
-		mockMvc.perform(get("/recipe/1/show")).andExpect(status().isOk()).andExpect(view().name("recipe/show"))
-				.andExpect(model().attributeExists("recipe"));
+		mockMvc.perform(get("/recipe/1/show"))
+			.andExpect(status().isOk())
+			.andExpect(view().name("recipe/show"))
+			.andExpect(model().attributeExists("recipe"));
 	}
 
-	@Test
-	public void testGetRecipeNumberFormatException() throws Exception {
+    @Test
+    public void testGetRecipeNumberFormatException() throws Exception {
 
-		mockMvc.perform(get("/recipe/asdf/show"))
-			.andExpect(status().isBadRequest())
-			.andExpect(view().name("400error"));
-	}
-	
+        mockMvc.perform(get("/recipe/asd/show"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("400error"));
+    }
 	
 }
